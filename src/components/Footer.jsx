cdef8b225e3dfd8bf2de85fddc34f55b7d969b59
z3tz3r0 <kittipan.wang@gmail.com>
2025-04-07 14:57:37 +0700

    fix(syntax): fix the syntax using className instead

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,32 +5,32 @@ const Footer = () => {
     return (
         <div className=''>
             <footer className='flex flex-col justify-center p-4 sm:px-16'>
-                <div className='sm:flex justify-between sm:items-start sm:mt-5'>
+                <div className='justify-between sm:flex sm:items-start sm:mt-5'>
                     <figure className='flex items-center mb-15'>
                         <img className='w-6' src="/assets/logo-all_rice-white.svg" alt="All Rice Logo" />
-                        <p class="logo-text text-[var(--clr-primary)]">All Rice</p>
+                        <p className="logo-text text-[var(--clr-primary)]">All Rice</p>
                     </figure>
                     <div className='sm:w-sm'>
                         <ul className='flex justify-between border-b-1 border-[var(--clr-primary)]'>
                             <li className='footer-menu'><NavLink to='/'>หน้าหลัก</NavLink></li>
-                            <li className='footer-menu mx-4'><NavLink to='products'>ผลิตภัณฑ์</NavLink></li>
-                            <li className='footer-menu mr-4'><NavLink to='about'>เกี่ยวกับเรา</NavLink></li>
+                            <li className='mx-4 footer-menu'><NavLink to='products'>ผลิตภัณฑ์</NavLink></li>
+                            <li className='mr-4 footer-menu'><NavLink to='about'>เกี่ยวกับเรา</NavLink></li>
                             <li className='footer-menu'><NavLink to='contact'>ติดต่อเรา</NavLink></li>
                         </ul>
                         <div className='flex justify-end mt-4 mb-15'>
                             <NavLink to='cart' >
-                                <i class="ph ph-facebook-logo text-3xl text-white mx-5"></i>
+                                <i className="mx-5 text-3xl text-white ph ph-facebook-logo"></i>
                             </NavLink>
                             <NavLink to='cart' >
-                                <i class="ph ph-tiktok-logo text-3xl text-white"></i>
+                                <i className="text-3xl text-white ph ph-tiktok-logo"></i>
                             </NavLink>
                         </div>
                     </div>
-                </div>   
+                </div>
                 <div className='flex justify-between border-t-1 border-[var(--clr-gray)] h-6 p-2 mb-2'>
                     <p className='footer-text'>allrice.com</p>
                     <div className='flex'>
-                        <p className='footer-text mx-5'>นโยบายและความเป็นส่วนตัว</p>
+                        <p className='mx-5 footer-text'>นโยบายและความเป็นส่วนตัว</p>
                         <p className='footer-text'>ข้อกำหนดและเงื่อนไข</p>
                     </div>
                 </div>
