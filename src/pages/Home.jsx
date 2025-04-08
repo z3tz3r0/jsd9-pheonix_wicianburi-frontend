17aab9005d7063096a900490f383899be0b66e88
Yanisa <kadyanisa@gmail.com>
2025-04-08 15:25:35 +0700

    add homepage section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
+import { Typography } from '@mui/material'
+import ButtonAddToCart from '../components/ButtonAddToCart'
+
 
 const Home = () => {
     return (
         <div>
-            <section className='overflow-hidden h-screen'>
-                <figure className='hidden sm:flex justify-center fixed top-0 right-0 w-[480px] h-[500px] bg-[var(--clr-accent)] rounded-b-full z-0 mr-[300px]'>
-                    <img src="/assets/homepage-img.png" alt="" className='z-10'/>
-                </figure>
+            <section className='h-screen outline'>
+                {/* <figure className='hidden sm:flex relative overflow-hidden -z-0'>
+                    <figure className='fixed w-[480px] h-[500px] bg-accent rounded-b-full top-0 right-0'>
+                        <img src="/assets/homepage-img.png" alt=""/>
+                    </figure>
+                </figure> */}
+            </section>
+
+            <section className='flex flex-col items-center h-[560px]'>
+                <Typography variant="h3" component="h2">สินค้าขายดี</Typography>
+            </section>
+
+            <section className='h-[560px]'>
+
+            </section>
+
+            <section>
+                <div className='flex flex-col items-center'>
+                    <Typography component="h3" sx={{fontSize: '1.25rem'}}>ข้าวหอมมะลิ</Typography>
+                    <ButtonAddToCart />
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Typography component="h3" sx={{fontSize: '1.25rem'}}>ข้าวเหนียว</Typography>
+                    <ButtonAddToCart />
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Typography component="h3" sx={{fontSize: '1.25rem'}}>ข้าวขาว</Typography>
+                    <ButtonAddToCart />
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Typography component="h3" sx={{fontSize: '1.25rem'}}>ข้าวเพื่อสุขภาพ</Typography>
+                    <ButtonAddToCart />
+                </div>
+            </section>
+
+            <section className='flex flex-col items-center h-[560px]'>
+                <Typography variant="h3" component="h2">ผลิตภัณฑ์แปรรูปจากข้าว</Typography>
+            </section>
+
+            <section>
+                <p>Free Delivery</p>
+                <p>On all order over ฿1000</p>
             </section>
         </div>
     )
