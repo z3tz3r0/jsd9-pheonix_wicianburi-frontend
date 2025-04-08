a898a5511f244ec4c6c7edd5cc23d2323949773e
Yanisa <kadyanisa@gmail.com>
2025-04-08 13:24:43 +0700

    create figure hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <section className='overflow-hidden h-screen'>
+                <figure className='hidden sm:flex justify-center fixed top-0 right-0 w-[480px] h-[500px] bg-[var(--clr-accent)] rounded-b-full z-0 mr-[300px]'>
+                    <img src="/assets/homepage-img.png" alt="" className='z-10'/>
+                </figure>
+            </section>
+        </div>
     )
 }
 
