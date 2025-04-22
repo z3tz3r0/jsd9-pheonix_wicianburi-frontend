34098479834b085b04e2e09f90b0895fe793629d
z3tz3r0 <kittipan.wang@gmail.com>
2025-04-22 09:25:27 +0700

    fix(route): change route from link to endpoint directly to link index

--- src/components/account/SidebarAccount.jsx
+++ src/components/account/SidebarAccount.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { NavLink } from 'react-router'
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import React from 'react';
+import { NavLink } from 'react-router';
 
 // ยังไม่ฟังก์ชั่น รอปรับแก้
 const SidebarAccount = () => {
@@ -10,14 +10,14 @@ const SidebarAccount = () => {
       <div className="p-4 rounded-md shadow-lg bg-primary w-52">
         <ul>
           <li className="flex items-center py-2 hover:text-accent">
-            <NavLink to="account">
-              <AccountCircleOutlinedIcon className='mr-2'/>
+            <NavLink to=".">
+              <AccountCircleOutlinedIcon className='mr-2' />
               บัญชีของฉัน
             </NavLink>
           </li>
           <li className="flex items-center py-2 hover:text-accent">
             <NavLink to="order-history">
-              <ShoppingBagOutlinedIcon className='mr-2'/>
+              <ShoppingBagOutlinedIcon className='mr-2' />
               การซื้อของฉัน
             </NavLink>
           </li>
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import AccountContainer from '../containers/AccountContainer'
 import SidebarAccount from '../components/account/SidebarAccount'
+import AccountContainer from '../containers/AccountContainer'
+
 import TabbarAccount from '../components/account/TabbarAccount'
 
 const Account = () => {
-    return (
-        <div className='flex justify-center'>
-            <div className="flex flex-col sm:flex-row sm:justify-between w-7xl">
-                <SidebarAccount />
-                <TabbarAccount />
-                <AccountContainer />
-            </div>
-        </div>
-    )
+  return (
+    <div className='flex justify-center'>
+      <div className="flex flex-col sm:flex-row sm:justify-between w-7xl">
+        <SidebarAccount />
+        <TabbarAccount />
+        <AccountContainer />
+      </div>
+    </div>
+  )
 }
 
 export default Account
