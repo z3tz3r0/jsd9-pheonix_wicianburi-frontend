7431ffab72b1b570c5382db4470bdbbee7ae1126
z3tz3r0 <kittipan.wang@gmail.com>
2025-04-23 14:44:19 +0700

    feat(orderDetail): add page order detail page

--- src/pages/OrderDetail.jsx
+++ src/pages/OrderDetail.jsx
@@ -6,86 +6,73 @@ const OrderDetail = () => {
   // const orderId = useParams();
   // console.log(orderId)
 
-  // Trying render order history using hard code
+  // TODO: GET order details from data base
+  // Required useEffect to fetch order details from data base using useParams
 
-  /* const orderDetails = [
+  // render order history using hard code
+  const orderDetails = [
     {
       id: 1,
       image: "../../public/assets/ข้าวหอมมะลิ กข15.webp",
       name: "ข้าวหอมมะลิ 105",
-      itemVariant: 1,
+      itemVariant: "1 กิโลกรัม",
       price: 100,
       quantity: 1,
-      subtotal: this.price * this.quantity,
     },
     {
       id: 2,
       image: "../../public/assets/ข้าวมันปู.webp",
       name: "ข้าวมันปู",
-      itemVariant: 1,
+      itemVariant: "1 กิโลกรัม",
       price: 150,
       quantity: 2,
-      subtotal: this.price * this.quantity,
     },
     {
       id: 3,
       image: "../../public/assets/ข้าวกล้อง.webp",
       name: "ข้าวกล้อง",
-      itemVariant: 1,
+      itemVariant: "1 กิโลกรัม",
       price: 120,
       quantity: 3,
-      subtotal: this.price * this.quantity,
     },
-  ] */
+  ]
 
-  // TODO: GET order details from data base
+  // Calculation
+  const totalItemsPrice = orderDetails.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  const shippingCost = 50;
+  const grandTotal = totalItemsPrice + shippingCost;
 
 
   return (
-    <div className='flex gap-8 mx-auto max-w-7xl outline-1 **:outline-red-500 **:outline'>
-
-      <div>
-        <h1 className='text-4xl font-bold'>รายละเอียดคำสั่งซื้อ</h1>
-        <table className='text-center w-xl'>
-          <thead>
-            <tr>
-              <th className='outline-1'>รายการสินค้า</th>
-              <th className='outline-1'></th>
-              <th className='outline-1'>ราคาต่อชิ้น</th>
-              <th className='outline-1'>จำนวน</th>
-              <th className='outline-1'>ราคารวม</th>
+    <div className='flex flex-col w-full max-w-5xl gap-8 px-4 mx-auto sm:px-8 lg:pl-36 outline-1'>
+      <h1 className='text-4xl font-bold'>รายละเอียดคำสั่งซื้อ</h1>
+      <table className='w-full max-w-5xl text-center bg-gray-100 rounded-lg *:text-sm'>
+        <thead>
+          <tr className='h-12 *:p-4 *:font-normal'>
+            <th className='text-left w-30'>รายการสินค้า</th>
+            <th className='text-left'></th>
+            <th>ราคา</th>
+            <th>จำนวน</th>
+            <th>รวม</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orderDetails.map(item => (
+            <tr className='border-t-1' key={item.id}>
+              <td className='p-4 pr-0'><img className='rounded-2xl' src={item.image} alt={item.name} /></td>
+              <td className='pl-4 text-left'><span className='font-bold'>{item.name}</span><br /><span>{item.itemVariant}</span></td>
+              <td>฿{item.price}</td>
+              <td>{item.quantity}</td>
+              <td>฿{item.price * item.quantity}</td>
             </tr>
-          </thead>
-          <tbody>
-            {/* {orderDetails.map(item => (
-              <tr key={item.id}>
-                <td><img src={item.image} alt={item.name} /></td>
-                <td><span>{item.name}</span><br /><span>{item.itemVariant}</span></td>
-                <td>฿{item.price}</td>
-                <td>{item.quantity}</td>
-                <td>{item.subtotal}</td>
-              </tr>
-            ))} */}
-            <tr>
-              <td><img className='w-24 rounded-2xl' src="/assets/ข้าวหอมมะลิปทุมธานี.webp" /></td>
-              <td><span>test</span><br /><span>test</span></td>
-              <td>฿100</td>
-              <td>1</td>
-              <td>200</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <td>{""}</td>
-            <td>{""}</td>
-            <td>ค่าขนส่ง: <span>฿50</span></td>
-            <td>{""}</td>
-          </tfoot>
-        </table>
-        <hr />
+          ))}
+        </tbody>
+      </table>
+      <p className='w-[75%] text-right'>ค่าขนส่ง:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className='font-bold'>฿{shippingCost}</span></p>
+      <hr />
+      <p className='w-[75%] text-right'>ราคารวม:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className='font-bold'>฿{grandTotal}</span></p>
 
-      </div>
-
-    </div>
+    </div >
   )
 }
 
