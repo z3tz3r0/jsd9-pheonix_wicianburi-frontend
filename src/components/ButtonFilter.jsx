fcea3e271e78e5835a35d4d582147aed2379d8a2
Yanisa <kadyanisa@gmail.com>
2025-04-24 21:43:25 +0700

    created buttonfilter and backtotop

--- src/components/ButtonFilter.jsx
+++ src/components/ButtonFilter.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router";
 
 const ButtonFilter = ({ children, className }) => {
     return (
-        <Link to="" >
+        // <Link key={category} to={`/products/${category}`}>
+        <Link to="products" >
             <Button
                 className={`w-full font-semibold bg-[#01c9ac] rounded-full cursor-pointer sm:text-2xl sm:p-6 hover:bg-black/90 active:shadow-md ${className}`}        >
                 {children}
