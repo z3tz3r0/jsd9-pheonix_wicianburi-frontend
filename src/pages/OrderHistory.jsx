61b9bc6a2934fac56213a20705869682828c6bf6
tanida <tanida.manee@gmail.com>
2025-04-25 09:34:13 +0700

    feat(account): modified SidebarAccount.jsx and TabbarAccount.jsx

--- src/pages/OrderHistory.jsx
+++ src/pages/OrderHistory.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const OrderHistory = () => {
-    return (
-        <div>OrderHistory</div>
-    )
+
+
+  return (
+    <main className="px-4 sm:w-4xl">
+        <h1 className="hidden mb-8 text-4xl font-bold sm:block">
+        การซื้อของฉัน
+        </h1>
+        <div className="w-full text-center bg-gray-200 rounded-lg h-100">
+            ตาราง
+        </div>
+    </main>
+)
 }
 
 export default OrderHistory
