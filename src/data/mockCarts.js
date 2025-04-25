35aead77262e1351675c75084072c7da203c4be1
Yanisa <kadyanisa@gmail.com>
2025-04-25 18:06:18 +0700

    edited mockcart

--- src/data/mockCarts.js
+++ src/data/mockCarts.js
@@ -1,25 +1,25 @@
 export const carts = [
   {
     id: 1,
-    name: 'ข้าวหอมมะลิ',
-    weight: 5,
-    price: 33.9,
-    quantity: 1,
+    name: 'ข้าวหอมมะลิ กข15',
+    weight: 1,
+    price: 40,
+    quantity: 2,
     image: '/assets/rice-images/jasmine-rice.jpg'
   },
   {
     id: 2,
-    name: 'ข้าวขมิ้น',
+    name: 'ข้าวเหลืองประทิวชุมพร',
     weight: 1,
-    price: 14.9,
+    price: 28,
     quantity: 1,
     image: '/assets/rice-images/brown-rice.jpg'
   },
   {
     id: 3,
     name: 'ข้าวไรซ์เบอร์รี่',
-    weight: 1,
-    price: 16.9,
+    weight: 5,
+    price: 500,
     quantity: 1,
     image: '/assets/rice-images/riceberry.jpg'
   }
