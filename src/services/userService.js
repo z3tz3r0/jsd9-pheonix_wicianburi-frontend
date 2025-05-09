7cbeeed005b38336462b03860708b61f3a797825
z3tz3r0 <kittipan.wang@gmail.com>
2025-05-09 18:27:00 +0700

    fix(vercel): config vercel path

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,13 +1,23 @@
 import api from "./api";
 
 export const loginUser = async (loginData) => {
-  const res = await api.post("/auth/login", loginData, {
+  const res = await api.post("api/auth/login", loginData, {
     withCredentials: true,
   });
   return res.data;
 };
 
 export const registerUser = async (registerData) => {
-  const res = await api.post("/auth/register", registerData);
+  const res = await api.post("api/auth/register", registerData);
   return res.data;
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const res = await api.get("/api/auth/users/me");
+    return res.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 };
