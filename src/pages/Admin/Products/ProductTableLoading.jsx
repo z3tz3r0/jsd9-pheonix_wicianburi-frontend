3a6e979313b8ecc4e623fb76e5597a1e14b21ae7
z3tz3r0 <kittipan.wang@gmail.com>
2025-05-11 12:10:38 +0700

    style(admin): add loading stlying and fetch data from db

---
+++ src/pages/Admin/Products/ProductTableLoading.jsx
@@ -0,0 +1,35 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  TableCell,
+  TableRow
+} from '@/components/ui/table';
+
+const ProductTableLoading = ({ index }) => {
+  return (
+    <TableRow key={`skeleton-${index}`}>
+      <TableCell>
+        <Skeleton className="w-24 h-5" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="w-40 h-5" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="w-20 h-5" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="w-24 h-5" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="w-20 h-5" />
+      </TableCell>
+      <TableCell>
+        <div className="flex justify-center gap-2">
+          <Skeleton className="w-8 h-8 rounded-md" />
+          <Skeleton className="w-8 h-8 rounded-md" />
+        </div>
+      </TableCell>
+    </TableRow>
+  )
+}
+
+export default ProductTableLoading
