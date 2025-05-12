6838c027e07df6f2221a670a4b76ea5e78fc5b36
z3tz3r0 <kittipan.wang@gmail.com>
2025-05-12 20:10:11 +0700

    feat(payment): upload image for payment confirmation

--- src/components/UploadButton.jsx
+++ src/components/UploadButton.jsx
@@ -1,35 +1,65 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Button from '@mui/material/Button';
+import { useEffect, useRef, useState } from 'react';
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
-  height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  whiteSpace: 'nowrap',
-  width: 1,
-});
 
 export default function InputFileUpload() {
+  const cloudinaryRef = useRef();
+  const widgetRef = useRef();
+  const [uploadedAssetInfo, setUploadedAssetInfo] = useState(null);
+  const [uploadError, setUploadError] = useState(null);
+
+  cloudinaryRef.current = window.cloudinary;
+
+  useEffect(() => {
+    if (cloudinaryRef.current) {
+      widgetRef.current = cloudinaryRef.current.createUploadWidget({
+        cloudName: "djalxl4dp",
+        uploadPreset: "ml_default",
+      }, (error, result) => {
+        if (error) {
+          setUploadError("Cloudinary Upload Error: ", error);
+          setUploadedAssetInfo(null);
+        } else if (result && result.event === "success") {
+          setUploadedAssetInfo(result.info);
+          console.log(result.info)
+          setUploadError(null);
+        }
+      });
+    } else {
+      setUploadError("Cloud not initialize image upload service.");
+    }
+  }, []);
   return (
-    <Button
-      component="label"
-      role={undefined}
-      variant="contained"
-      tabIndex={-1}
-      startIcon={<CloudUploadIcon />}
-    >
-      Upload files
-      <VisuallyHiddenInput
-        type="file"
-        onChange={(event) => console.log(event.target.files)}
-        multiple
-      />
-    </Button>
-  );
+    <>
+      <Button
+        component="label"
+        role={undefined}
+        variant="contained"
+        tabIndex={-1}
+        onClick={() => {
+          setUploadedAssetInfo(null);
+          setUploadError(null);
+          if (widgetRef.current) {
+            widgetRef.current.open();
+          } else {
+            setUploadError("Image Upload service is not ready.");
+          }
+        }}
+        startIcon={<CloudUploadIcon />}
+      >
+        อัปโหลดไฟล์
+      </Button>
+      {uploadedAssetInfo && (
+        <div className='mt-4 text-green-500'>
+          <p>อัปโหลดสำเร็จ {uploadedAssetInfo.original_filename}</p>
+        </div>
+      )}
+      {uploadError && (
+        <div className='mt-4 text-red-500'>
+          <p>{uploadError}</p>
+        </div>
+      )}
+    </>
+  )
 }
